b1cffe6a5820f50f87a52f7748527323a77009d4
wangran <328106180@qq.com>
2019-08-07 20:04:34 +0800

    1

--- src/main/java/com/acitivi/App.java
+++ src/main/java/com/acitivi/App.java
@@ -3,15 +3,16 @@ package com.acitivi;
 import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 //@EnableAutoConfiguration
-@ComponentScan(value = {"com.acitivi" })
+@ComponentScan(value = { "com.acitivi" })
 @MapperScan(basePackages = "com.acitivi.dao")
 public class App {
-    
+
     public static void main( String[] args ){
         SpringApplication.run(App.class, args);
     }
